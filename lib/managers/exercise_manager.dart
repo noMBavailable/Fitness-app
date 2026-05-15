f5ae3a2bf655871be5ddd9eac710fa5d0e0fbbbe
noMBavailable <152862820+noMBavailable@users.noreply.github.com>
2026-05-15 08:58:14 +0200

    patch 0.25_8
    Updated scripts:
    Exercise_manager.dart
    Workout_manager.dart
    
    > Exercise_manager.dart:
    Added premade exercises
    
    > Workout_manager.dart:
    Added premade workouts

--- lib/managers/exercise_manager.dart
+++ lib/managers/exercise_manager.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import '../models/exercise_model.dart';
 
 class ExerciseManager extends ChangeNotifier {
-  final List<Exercise> _exercises = [];
+  // We initialize the list with premade data instead of leaving it empty
+  final List<Exercise> _exercises = [
+    Exercise(id: 'pre_1', name: 'Bench Press', reps: 10, weight: 60.0),
+    Exercise(id: 'pre_2', name: 'Squats', reps: 8, weight: 80.0),
+    Exercise(id: 'pre_3', name: 'Deadlift', reps: 5, weight: 100.0),
+    Exercise(id: 'pre_4', name: 'Lat Pulldown', reps: 12, weight: 45.0),
+    Exercise(id: 'pre_5', name: 'Shoulder Press', reps: 10, weight: 30.0),
+    Exercise(id: 'pre_6', name: 'Bicep Curls', reps: 12, weight: 15.0),
+  ];
 
   List<Exercise> get exercises => _exercises;
 
@@ -13,7 +21,7 @@ class ExerciseManager extends ChangeNotifier {
       reps: reps,
       weight: weight,
     ));
-    notifyListeners(); // This tells the UI to refresh
+    notifyListeners(); 
   }
 
   void deleteExercise(String id) {
@@ -24,7 +32,12 @@ class ExerciseManager extends ChangeNotifier {
   void updateExercise(String id, String newName, int newReps, double newWeight) {
     int index = _exercises.indexWhere((ex) => ex.id == id);
     if (index != -1) {
-      _exercises[index] = Exercise(id: id, name: newName, reps: newReps, weight: newWeight);
+      _exercises[index] = Exercise(
+        id: id, 
+        name: newName, 
+        reps: newReps, 
+        weight: newWeight
+      );
       notifyListeners();
     }
   }
--- lib/managers/workout_manager.dart
+++ lib/managers/workout_manager.dart
@@ -3,7 +3,36 @@ import '../models/workout_model.dart';
 import '../models/exercise_model.dart';
 
 class WorkoutManager extends ChangeNotifier {
-  final List<Workout> _workouts = [];
+  // Initializing with premade workouts
+  final List<Workout> _workouts = [
+    Workout(
+      id: 'w_pre_1',
+      name: 'Full Body A',
+      selectedExercises: [
+        Exercise(id: 'e1', name: 'Squats', reps: 8, weight: 80.0),
+        Exercise(id: 'e2', name: 'Bench Press', reps: 10, weight: 60.0),
+        Exercise(id: 'e3', name: 'Lat Pulldown', reps: 12, weight: 45.0),
+      ],
+    ),
+    Workout(
+      id: 'w_pre_2',
+      name: 'Push Day (Chest/Shoulders)',
+      selectedExercises: [
+        Exercise(id: 'e4', name: 'Incline DB Press', reps: 10, weight: 24.0),
+        Exercise(id: 'e5', name: 'Shoulder Press', reps: 10, weight: 30.0),
+        Exercise(id: 'e6', name: 'Tricep Pushdowns', reps: 15, weight: 20.0),
+      ],
+    ),
+    Workout(
+      id: 'w_pre_3',
+      name: 'Leg Day (Focus Quads)',
+      selectedExercises: [
+        Exercise(id: 'e7', name: 'Leg Press', reps: 12, weight: 120.0),
+        Exercise(id: 'e8', name: 'Leg Extensions', reps: 15, weight: 40.0),
+        Exercise(id: 'e9', name: 'Calf Raises', reps: 20, weight: 50.0),
+      ],
+    ),
+  ];
 
   List<Workout> get workouts => _workouts;
 
@@ -24,6 +53,4 @@ class WorkoutManager extends ChangeNotifier {
   void notifyUI() {
     notifyListeners();
   }
-
- 
 }
